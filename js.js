c374ab097321a079fd750c20d650929a8c09a86e
Javier Farre <jfarre90@gmail.com>
2019-02-22 10:26:48 +0000

    written the code with basic styling

---
+++ js.js
@@ -0,0 +1,11 @@
+// http://aws.random.cat/meow
+
+$("#btn").click(function(){
+  $.getJSON("https://random.cat/meow")
+  .done(function(data){
+    $('#catImg').attr("src", data.file);
+  })
+  .fail(function(){
+    alert("REQUEST IS NOT PAWSIBBLE");
+  })
+})
